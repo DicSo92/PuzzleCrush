87835216fc7844e35ac510acb136f4f584528a8b
Luzzi Charly <luzzi.charly@gmail.com>
2021-04-09 21:07:35 +0200

    Game page && onClick new game

--- app/src/main/java/com/game/puzzlecrush/MainActivity.java
+++ app/src/main/java/com/game/puzzlecrush/MainActivity.java
@@ -2,13 +2,29 @@ package com.game.puzzlecrush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button newGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.newGame = (Button) findViewById(R.id.btn_newGame);
+
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newGameActivity = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(newGameActivity);
+                finish();
+            }
+        });
     }
 }
